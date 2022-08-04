97a846bedb66ed21acbbe87eb5a18154a0d8e722
QueenJammie <jammie.lacc@outlook.com>
2022-08-04 15:35:47 -0400

    Added some events

--- src/Sessionete.js
+++ src/Sessionete.js
@@ -10,7 +10,13 @@ export default function Sessionete()
           Entre temps, venez nous voir en spectacle:</p>
           <ul>
             <li className="noListStyle"><a href="https://www.facebook.com/events/1245212046251233" alt="Festival Gladiamort sur Facebook">30 juillet à 18h30 au Festival Médiéval Gladiamort 2022</a></li>
+            <br />
             <li className="noListStyle"><a href="https://www.facebook.com/events/1409916459474907" alt="La troupe au Festival des Traditions du Monde de Sherbrooke sur Facebook">7 Août entre 14h et 16h au Grand Défilé Anniversaire du Festival des Traditions du Monde</a></li>
+            <br />
+            <li className="noListStyle"><a href="https://ftms.ca/espace-orient/" alt="Festival des Traditions du Monde, Espace Orient">10 Août 18h15 au Festival des traditions du Monde, Espace Orient</a></li>
+            <li className="noListStyle"><a href="https://ftms.ca/espace-orient/" alt="Festival des Traditions du Monde, Espace Orient">12 Août 14h30 DÉMO et ATELIER DANSES ORIENTALES au Festival des traditions du Monde, Espace Orient</a></li>
+            <li className="noListStyle"><a href="https://ftms.ca/espace-orient/" alt="Festival des Traditions du Monde, Espace Orient">13 Août 12h au Festival des traditions du Monde, Espace Orient</a></li>
+            <li className="noListStyle"><a href="https://ftms.ca/espace-orient/" alt="Festival des Traditions du Monde, Espace Orient">14 Août 12h au Festival des traditions du Monde, Espace Orient</a></li>
           </ul>
           <p className="memo">Nos services ne sont pas assujetis aux taxes!</p>
       </div>
