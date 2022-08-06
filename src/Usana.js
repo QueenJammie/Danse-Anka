39d8f8214e39f2d73de89589a2b88d26d1e7f369
QueenJammie <jammie.lacc@outlook.com>
2022-08-06 19:40:30 -0400

    Made the button display responsive in Usana

--- src/Usana.js
+++ src/Usana.js
@@ -6,7 +6,7 @@ export default function Usana()
 {
   return (
     <div className="Usana row" id="usana">
-      <div className="col-7">
+      <div className="col-12 col-sm-7">
         <h2>La santé par la nutrition Usana</h2>
         <ul>
           <li>Un kiosque de produits santés est disponible au studio!</li>
@@ -14,7 +14,7 @@ export default function Usana()
           <li>Visite de la boutique virtuelle!</li>
         </ul>
       </div>
-      <div className="col-5">
+      <div className="col-12 col-sm-5">
         <a href="https://anka.usana.com/ux/dotcom/fre-CA/home" alt="" target="_blank" rel="noreferrer" >
           <button className="btn btn-primary button">
             VISITER LA BOUTIQUE
