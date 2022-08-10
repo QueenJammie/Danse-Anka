749d23b2130551215d53ece9fb3df91e5db85acc
QueenJammie <jammie.lacc@outlook.com>
2022-08-10 15:25:11 -0400

    Added a current date

--- src/App.js
+++ src/App.js
@@ -13,6 +13,44 @@ import './App.css';
 
 export default function App() 
 {
+  var today = new Date();
+
+  if ((today.getMonth() + 1) === 1) {
+      var date = `${today.getDate()} Janvier ${today.getFullYear()} | ${today.getHours()}:${today.getMinutes()}`;
+  }
+  if ((today.getMonth() + 1) === 2) {
+      date = `${today.getDate()} Février ${today.getFullYear()} | ${today.getHours()}:${today.getMinutes()}`;
+  }
+  if ((today.getMonth() + 1) === 3) {
+      date = `${today.getDate()} Mars ${today.getFullYear()} | ${today.getHours()}:${today.getMinutes()}`;
+  }
+  if ((today.getMonth() + 1) === 4) {
+      date = `${today.getDate()} Avril ${today.getFullYear()} | ${today.getHours()}:${today.getMinutes()}`;
+  }
+  if ((today.getMonth() + 1) === 5) {
+      date = `${today.getDate()} Mai ${today.getFullYear()} | ${today.getHours()}:${today.getMinutes()}`;
+  }
+  if ((today.getMonth() + 1) === 6) {
+      date = `${today.getDate()} Juin ${today.getFullYear()} | ${today.getHours()}:${today.getMinutes()}`;
+  }
+  if ((today.getMonth() + 1) === 7) {
+      date = `${today.getDate()} Juillet ${today.getFullYear()} | ${today.getHours()}:${today.getMinutes()}`;
+  }
+  if ((today.getMonth() + 1) === 8) {
+      date = `${today.getDate()} Août ${today.getFullYear()} | ${today.getHours()}:${today.getMinutes()}`;
+  }
+  if ((today.getMonth() + 1) === 9) {
+      date = `${today.getDate()} Septembre ${today.getFullYear()} | ${today.getHours()}:${today.getMinutes()}`;
+  }
+  if ((today.getMonth() + 1) === 10) {
+      date = `${today.getDate()} Octobre ${today.getFullYear()} | ${today.getHours()}:${today.getMinutes()}`;
+  }
+  if ((today.getMonth() + 1) === 11) {
+      date = `${today.getDate()} Novembre ${today.getFullYear()} | ${today.getHours()}:${today.getMinutes()}`;
+  }
+  if ((today.getMonth() + 1) === 12) {
+      date = `${today.getDate()} Décembre ${today.getFullYear()} | ${today.getHours()}:${today.getMinutes()}`;
+  }
   return (
     <div className="App" id="mainHeader">
       <div className="container">
@@ -35,7 +73,8 @@ export default function App()
           <a href="https://www.facebook.com/danseanka/" alt="Contacter Danse Anka" target="_blank" rel="noreferrer"><img src={Messenger} alt="" className="messenger" /></a>
         </body>
         <footer className="App-footer">
-          <p><a href="https://github.com/QueenJammie/Danse-Anka/tree/main/src" target="_blank" rel="noreferrer">Open Code-Source</a> by <a href="mailto:jammie.lacc@outlook.com">Jammie Lachance-Côté</a></p>
+          <p>{date}<br />
+            <a href="https://github.com/QueenJammie/Danse-Anka/tree/main/src" target="_blank" rel="noreferrer">Open Code-Source</a> by <a href="mailto:jammie.lacc@outlook.com">Jammie Lachance-Côté</a></p>
         </footer>
       </div>
     </div>
